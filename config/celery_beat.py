da21cc873e728020b9a088383660e5051ace11d0
Mohamed Mansor <mohamedmansor212@gmail.com>
2023-10-16 07:26:47 +0300

    [celerybeat] update schedule to be each 5 minutes

--- config/celery_beat.py
+++ config/celery_beat.py
@@ -5,6 +5,6 @@ def setup_periodic_tasks(app):
     app.conf.beat_schedule = {
         "refresh-feeds-and-posts": {
             "task": "rrs_reader.feed.tasks.periodic_update_feeds_task",
-            "schedule": crontab(minute="*/1"),  # every 5 minutes
+            "schedule": crontab(minute="*/5"),  # every 5 minutes
         },
     }
